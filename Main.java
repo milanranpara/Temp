2b8d2a40541a4061f62d2d3ad589e832be0f5899
mds208 <mds208@DCSRCC.com>
2023-10-03 10:49:54 +0530

    Spelling Change

--- Main.java
+++ Main.java
@@ -6,7 +6,7 @@ class Abc {
     }
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println(Abc.main("Hellio"));
+        System.out.println(Abc.main("Hello"));
         
     }
 }
