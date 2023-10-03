68d7175ac8c51cf9010d51b37b46a24455669be5
mds208 <mds208@DCSRCC.com>
2023-10-03 10:40:57 +0530

    initial commit

---
+++ Main.java
@@ -0,0 +1,12 @@
+
+class Abc {
+    public static int main(String args) {
+        System.out.println("Hello, World!");
+        return 0;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println(Abc.main("Hellio"));
+        
+    }
+}
